21856c52cdfbaf3e0da5f92c49dd87e75f0a3ab1
philipDilts <philip.dilts@gmail.com>
2016-06-06 17:53:36 -0400

    adding nested dictionary of review scores

--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -1,1 +1,6 @@
 #A dictionary of movie critics and their ratings of a small set of movies
+
+critics = {'Lisa Rose':   {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 
+    'Superman Returns': 5.0, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+    'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5}
+    }
